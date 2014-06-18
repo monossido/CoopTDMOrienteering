53834a07ed5bb88ca05a288344464e45e2a73d6b
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2014-06-19 00:39:38 +0200

    pronto per pubblicazione

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-CacciaAlTesoro1
+CoppTDMOrientiring
 ===============
 
-Caccia al tesoro villa Beatrice
+Orientiring a Villa Beatrice [http://www.museicollieuganei.it](http://www.museicollieuganei.it)
+
+Applicazione sviluppata da **Lorenzo Braghetto** (lorenzobraghetto.com) per **Cooperativa Terra di Mezzo**
+
+Licenza GPL - (see LICENSE)
+
+
+Software Open Source utilizzato:
+- [ActionBarSherlock](https://github.com/JakeWharton/ActionBarSherlock)
+- [CompassLibrary](https://github.com/monossido/CompassLibrary)
+
+Grafica creata da **Andrea Marson**
--- src/com/lorenzobraghetto/cooptdmorientiringvillabea/MainActivity.java
+++ src/com/lorenzobraghetto/cooptdmorientiringvillabea/MainActivity.java
@@ -3,8 +3,11 @@ package com.lorenzobraghetto.cooptdmorientiringvillabea;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,6 +20,7 @@ public class MainActivity extends SherlockActivity {
 
 	private Button ok;
 	private Button about;
+	private Button info;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -63,14 +67,33 @@ public class MainActivity extends SherlockActivity {
 			}
 		});
 
+		info = (Button) findViewById(R.id.info);
+		info.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setTitle(R.string.info)
+						.setMessage(R.string.testo_info);
+				builder.create().show();
+			}
+		});
+
 		about = (Button) findViewById(R.id.about);
 		about.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
+				PackageInfo pInfo = null;
+				try {
+					pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+				} catch (NameNotFoundException e) {
+					e.printStackTrace();
+				}
+				String version = pInfo.versionName;
 				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 				builder.setTitle(R.string.about)
-						.setMessage(R.string.testo_About);
+						.setMessage(Html.fromHtml(getString(R.string.testo_About) + " " + version));
 				builder.create().show();
 			}
 		});
